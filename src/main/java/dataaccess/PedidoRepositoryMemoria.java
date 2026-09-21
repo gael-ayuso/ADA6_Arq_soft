fe8d1ba70d47410f923c4331fcf7fb353dc140b8
Gael Ayuso <a21201424@alumnos.uady.mx>
2026-09-21 10:51:04 -0600

    Refactor `PedidoRepositoryMemoria` to use `Map` directly, removing `BaseDatosMemoria`.

--- src/main/java/dataaccess/PedidoRepositoryMemoria.java
+++ src/main/java/dataaccess/PedidoRepositoryMemoria.java
@@ -6,21 +6,20 @@ import data.BaseDatosMemoria;
 import java.util.Map;
 
 public class PedidoRepositoryMemoria implements PedidoRepository {
-    private final BaseDatosMemoria baseDatosMemoria;
+    private final Map<Integer, Pedido> baseDatosMemoria;
 
-    public PedidoRepositoryMemoria(BaseDatosMemoria baseDatosMemoria) {
+    public PedidoRepositoryMemoria(Map<Integer, Pedido> baseDatosMemoria) {
         this.baseDatosMemoria = baseDatosMemoria;
     }
 
     @Override
     public int guardar(Pedido pedido) {
-        Map<Integer, Pedido> datos = this.baseDatosMemoria.pedidos();
-        datos.put(pedido.getId(), pedido);
+        baseDatosMemoria.put(pedido.getId(), pedido);
         return pedido.getId();
     }
 
     @Override
     public Pedido buscarPorId(int id) {
-        return this.baseDatosMemoria.pedidos().get(id);
+        return this.baseDatosMemoria.get(id);
     }
 }
