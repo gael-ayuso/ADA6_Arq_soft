9b7c7c5949fdd38da3987d73fd8811900abe3bff
criscnsn <santanacristopher765@gmail.com>
2026-09-21 23:46:47 -0600

    contrato

--- src/main/java/dataaccess/ProductoRepository.java
+++ src/main/java/dataaccess/ProductoRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 /**
  * Contrato para el acceso a datos del catalogo de productos.
- * Permite desacoplar el origen de datos (CSV, TXT, JSON, Base de Datos) de la logica de negocio.
  */
 public interface ProductoRepository {
     List<Producto> obtenerTodos();
